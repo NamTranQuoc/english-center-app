0982c28d16cf7ae13ec32bd9ed4aeaca91a40ef4
NamTran <nam.tran@zamo.io>
2022-04-04 17:10:16 +0700

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:week3/screen/Login.dart';
 import 'package:week3/screen/Main.dart';
 import 'package:week3/screen/Signup.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(
     MaterialApp(
       title: 'English center',
