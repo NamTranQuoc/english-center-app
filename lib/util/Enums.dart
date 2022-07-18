e937d7f0884276fa1b6a1a775c5c905d924a4c3d
NamTranQuoc <72292925+NamTranQuoc@users.noreply.github.com>
2022-07-18 15:34:08 +0700

    Update Enums.dart

--- lib/util/Enums.dart
+++ lib/util/Enums.dart
@@ -1,5 +1,5 @@
 class Common {
-  static String host = 'https://englishcenter-spkt-api.herokuapp.com';
+  static String host = 'https://ec-pmp.herokuapp.com';
   // static String host = 'http://10.0.2.2:8080';
   // static String host = 'http://192.168.43.23:8080';
   static String avatarDefault =
